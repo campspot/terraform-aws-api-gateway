558feb02f74678dca7ce45ed9b8c2401caeaf356
tomicole <oluwatomisin.oshadiya@campspot.com>
2022-08-13 16:23:37 -0600

    adjust naming convention

--- main.tf
+++ main.tf
@@ -5,18 +5,17 @@
 #Description : This terraform module is designed to generate consistent label names and tags
 #              for resources. You can use terraform-labels to implement a strict naming
 #              convention.
-//module "labels" {
-//  source  = "clouddrove/labels/aws"
-//  version = "0.15.0"
-//
-//  enabled     = var.enabled
-//  name        = var.name
-//  repository  = var.repository
-//  environment = var.environment
-//  managedby   = var.managedby
-//  label_order = var.label_order
-//  attributes  = var.attributes
-//}
+module "labels" {
+  source  = "clouddrove/labels/aws"
+  version = "0.15.0"
+
+  enabled     = var.enabled
+  repository  = var.repository
+  environment = var.environment
+  managedby   = var.managedby
+  label_order = var.label_order
+  attributes  = var.attributes
+}
 
 # Module      : Api Gateway
 # Description : Terraform module to create Api Gateway resource on AWS for creatng api.
